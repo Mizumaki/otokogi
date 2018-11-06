2853acc3c65aa54d1dc891bdeff93354c4dab20f
Mizumaki <statham.programming@gmail.com>
2018-11-06 16:31:15 +0900

    done settings

--- truffle.js
+++ truffle.js
@@ -1,3 +1,7 @@
+var HDWalletProvider = require("truffle-hdwallet-provider");
+var privateKey = process.env.KEY;
+var infuraApiKey = process.env.INFURA;
+
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
@@ -6,6 +10,17 @@ module.exports = {
       host: "127.0.0.1",
       port: 7545,
       network_id: "*"
+    },
+    mainnet: {
+      provider: function () {
+        return new HDWalletProvider(
+          privateKey,
+          "https://mainnet.infura.io/" + infuraApiKey
+        );
+      },
+      network_id: 1,
+      gas: 2000000,
+      gasPrice: 8000000000 // 8 Gwei
     }
   }
 };
